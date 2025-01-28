79f07c6cf5503463ed83bb47eaf0996a9c1e63da
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2025-01-28 12:29:24 +0200

    Update ScrappingService.js

--- services/scrapping/ScrappingService.js
+++ services/scrapping/ScrappingService.js
@@ -127,7 +127,7 @@ export const ScrappingService = async (url) => {
         itemData["Primary Title"] = info.unitTitle;
         itemData["Secondary Title"] = info.secondaryTitle;
         itemData["Description"] = info.description.unit.content;
-        itemData["Price per Night"] = Math.floor(
+        itemData["Price per Night"] = Math.ceil(
           info.price.rawPrice / exchangeRate
         );
         itemData["Location"] = info.locationTrailHeading.details;
